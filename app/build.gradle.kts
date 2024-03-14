15e96dd57964ed680b8f98a27ae59f6a7ea55d99
Sinair-Guimaraes <siniar_@hotmail.com>
2024-03-14 17:03:04 -0400

    Configuração Banco de Dados SQLite  GASOLINA VS ETANOL.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,5 +35,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.journeyapps:zxing-android-embedded:3.5.0")
 
 }
